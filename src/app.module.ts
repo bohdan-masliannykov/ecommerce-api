e902e33802f0a2ff990b38023274f575460a9194
Bohdan <bohdan.masliannykov@gmail.com>
2024-11-05 21:24:03 +0100

    refactor: test connect via WSL and postgress

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'ecommerce-api',
+      synchronize: true, //disable in prod mode
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
